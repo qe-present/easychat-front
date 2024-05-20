b9c1be613406a48ff377e579865a193029d954d3
qe-present <2664481691@qq.com>
2024-05-21 01:38:42 +0800

    add

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,24 +1,15 @@
 <script setup lang="ts">
-import Versions from './components/Versions.vue'
-
-const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+import { ElConfigProvider } from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import { reactive } from 'vue'
+const locale = zhCn
+const config = reactive({
+  max: 1
+})
 </script>
 
 <template>
-  <img alt="logo" class="logo" src="./assets/electron.svg" />
-  <div class="creator">Powered by electron-vite</div>
-  <div class="text">
-    Build an Electron app with
-    <span class="vue">Vue</span>
-  </div>
-  <p class="tip">Please try pressing <code>F12</code> to open the devTool</p>
-  <div class="actions">
-    <div class="action">
-      <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">Documentation</a>
-    </div>
-    <div class="action">
-      <a target="_blank" rel="noreferrer" @click="ipcHandle">Send IPC</a>
-    </div>
-  </div>
-  <Versions />
+  <el-config-provider :locale="locale" :message="config">
+    <router-view />
+  </el-config-provider>
 </template>
